23fb43290afb46b1ae129225cf6eeb74e3302472
donnyhyon <dhyon333@hotmail.co.uk>
2025-03-06 07:23:43 +0000

    add range check

--- pizza.py
+++ pizza.py
@@ -1,14 +1,13 @@
 import argparse
 
 p = argparse.ArgumentParser(prog='Pizza Dough Helper')
-p.add_mutually_exclusive_group()
 
 p.add_argument("-r", "--hydration", help= "hydration of final pizza dough", type=int, default=70)
 p.add_argument("-n", "--numberOfBalls", help= "Number of pizza balls you want to make", type=int, default=10)
 p.add_argument("-s", "--size", help="Size of balls", type=int, default=280)
 
-p.add_argument("-p", "--poolish", help="If you want to use poolish", type=int)
-p.add_argument("-b", "--biga", help="If you want to use biga", type=int)
+p.add_argument("-p", "--poolish", help="If you want to use poolish, 1-100", type=int)
+p.add_argument("-b", "--biga", help="If you want to use biga, 1-100", type=int)
 
 args = p.parse_args()
 
@@ -25,21 +24,23 @@ Honey       : {honey}g
     """)
 
 
+def check_in_range(value, min, max):
+    if (value > max or value < min ):
+        raise argparse.ArgumentTypeError(f"{value} is out of range, allowed values are {min}-{max}")
 # Main
 
 prefermentPercentage = 0
-
 if (args.poolish):
+    check_in_range(args.poolish,0,100)
     prefermentPercentage = args.poolish
-
 if (args.biga):
+    check_in_range(args.biga,0,100)
     prefermentPercentage = args.biga
 
 hydration = args.hydration
 numberOfballs = args.numberOfBalls
 size = args.size
 totalDough = numberOfballs * size
-
 decimalPoint = 1
 
 ratio = 100 + hydration
@@ -50,7 +51,10 @@ oil = round(flour * 0.01, decimalPoint)
 yeast = round(flour * 0.005, decimalPoint)
 honey = round(flour * 0.005, decimalPoint)
 
-
 if (prefermentPercentage == 0):
     print("No preferment")
     console_output(totalDough,hydration,flour,water,salt,oil,yeast,honey)
+
+if (args.biga):
+    biga = totalDough * prefermentPercentage
+    print(biga)
